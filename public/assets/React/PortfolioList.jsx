13d84c1e5bf4f543fa5e73c81c30c6df4362dd92
Valerie Smith <valerie.plante.11@gmail.com>
2023-11-09 21:54:22 -0500

    added running app

--- public/assets/React/PortfolioList.jsx
+++ public/assets/React/PortfolioList.jsx
@@ -9,8 +9,10 @@ export default function PortfolioList(props){
         <div className = "portfolio">
             <h3>Project Name: {props.name}</h3>
             <p>Framework: {props.framework}</p>
+            <div className = "links">
 {  props.live &&  <a href = {props.live} className = "github">View Live</a>}
 <a href = {props.link} className = "github">See on Github</a>
+            </div>
 <div>
 <img src = {props.img1} className = "portfolio--image" ></img>
 <img src = {props.img2} className = "portfolio--image" ></img>
--- public/assets/React/portfolio.js
+++ public/assets/React/portfolio.js
@@ -1,9 +1,10 @@
 export default [
     {
-        name: "Dice Game",
-        Framework: "React",
-        screenshots: ["../images/yahtzee/Screenshot 2023-10-16 215212.png", "../images/yahtzee/Screenshot 2023-10-16 215243.png", "../images/yahtzee/Screenshot 2023-10-16 215301.png"],
-        link: "https://github.com/Valerie3956/assignments/tree/main/level-4/themes"
+        name: "Run Tracker",
+        Framework: "MERN stack",
+        screenshots: ["../images/running/Screenshot 2023-11-09 213811.png", "../images/running/Screenshot 2023-11-09 213910.png", "../images/running/Screenshot 2023-11-09 213938.png"],
+        link: "https://github.com/Valerie3956/running",
+        live: "https://social-running-app.onrender.com/"
     },
     {
         name: "Recipes App",
@@ -18,5 +19,11 @@ export default [
         screenshots: ["../images/morning/Screenshot 2023-10-24 214149.png", "../images/morning/Screenshot 2023-10-24 214207.png", "../images/morning/Screenshot 2023-10-24 214229.png"],
         link: "https://github.com/Valerie3956/morning",
         live: "https://valerie3956morning.cyclic.app/"
+    },
+    {
+    name: "Dice Game",
+    Framework: "React",
+        screenshots: ["../images/yahtzee/Screenshot 2023-10-16 215212.png", "../images/yahtzee/Screenshot 2023-10-16 215243.png", "../images/yahtzee/Screenshot 2023-10-16 215301.png"],
+        link: "https://github.com/Valerie3956/assignments/tree/main/level-4/themes"
     }
 ]
