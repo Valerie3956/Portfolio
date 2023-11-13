a44b148e6922554361c289a0769f59fec292dbbc
Valerie Smith <valerie.plante.11@gmail.com>
2023-11-13 10:20:23 -0500

    reordered some apps

--- public/assets/React/portfolio.js
+++ public/assets/React/portfolio.js
@@ -7,18 +7,18 @@ export default [
         live: "https://social-running-app.onrender.com/"
     },
     {
-        name: "Recipes App",
-        Framework: "React",
-        screenshots: ["../images/Recipes/Screenshot 2023-10-17 103512.png", "../images/Recipes/Screenshot 2023-10-17 103536.png", "../images/Recipes/Screenshot 2023-10-17 103812.png"],
-        link: "https://github.com/Valerie3956/recipes",
-        // live: "https://valerie3956recipes.netlify.app/"
-    },
-    {
         name: "Morning Routine app",
         Framework: "MERN stack",
         screenshots: ["../images/morning/Screenshot 2023-10-24 214149.png", "../images/morning/Screenshot 2023-10-24 214207.png", "../images/morning/Screenshot 2023-10-24 214229.png"],
         link: "https://github.com/Valerie3956/morning",
         live: "https://valerie3956morning.cyclic.app/"
+    },
+    {
+        name: "Recipes App",
+        Framework: "React",
+        screenshots: ["../images/Recipes/Screenshot 2023-10-17 103512.png", "../images/Recipes/Screenshot 2023-10-17 103536.png", "../images/Recipes/Screenshot 2023-10-17 103812.png"],
+        link: "https://github.com/Valerie3956/recipes",
+        // live: "https://valerie3956recipes.netlify.app/"
     },
     {
     name: "Dice Game",
